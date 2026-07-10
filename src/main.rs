384405491e8ea5e411788fd89d1f7499f4fb603c
Artem Baguinski <abaguinski@gmail.com>
2026-07-10 18:29:06 +0000

    return Result

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::any::type_name;
+use thiserror::Error;
 
 #[expect(clippy::similar_names, reason = "there is a pattern to madness")]
-fn fib<F>(n: u8) -> F
+fn fib<F>(n: u8) -> Result<F, FibError>
 where
     F: num_traits::PrimInt + num_traits::Unsigned + MaxFibIndex,
 {
     if n == 0 {
-        return F::zero();
+        return Ok(F::zero());
     }
-    assert!(
-        n <= F::MAX_FIB_INDEX,
-        "Fibonacci number F({n}) won't fit in {}",
-        type_name::<F>()
-    );
+    if n > F::MAX_FIB_INDEX {
+        return Err(FibError::Overflow {
+            index: n,
+            type_name: type_name::<F>(),
+        });
+    }
     let two = F::from(2).expect("there can always be 2");
     let mut mask = 1 << (u8::BITS - n.leading_zeros() - 1);
 
@@ -22,13 +24,13 @@ where
         let fib_2k = fib_k * (two * fib_k_plus_one - fib_k);
         if mask == 1 && n & mask == 0 {
             // n = 2k, we already have the answer, avoid overfloating F(2k + 1)
-            return fib_2k;
+            return Ok(fib_2k);
         }
 
         let fib_2k_plus_one = fib_k * fib_k + fib_k_plus_one * fib_k_plus_one;
         if mask == 1 {
             // n = 2k + 1, we already have the answer, avoid overfloating F(2k + 2)
-            return fib_2k_plus_one;
+            return Ok(fib_2k_plus_one);
         }
 
         (fib_k, fib_k_plus_one) = if n & mask == 0 {
@@ -38,6 +40,12 @@ where
         };
         mask >>= 1;
     }
+}
+
+#[derive(Debug, Error)]
+enum FibError {
+    #[error("Fibonacci number F({index}) won't fit in {type_name}")]
+    Overflow { index: u8, type_name: &'static str },
 }
 
 trait MaxFibIndex {
@@ -67,7 +75,7 @@ impl MaxFibIndex for u128 {
 fn main() {
     use num_format::{Locale, ToFormattedString};
 
-    let n = u128::MAX_FIB_INDEX;
-    let fib_n: u128 = fib(n);
+    let n = u128::MAX_FIB_INDEX + 1;
+    let fib_n: u128 = fib(n).unwrap_or_else(|error| panic!("{error}"));
     println!("F({n}) = {}", fib_n.to_formatted_string(&Locale::en));
 }
