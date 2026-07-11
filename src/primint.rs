095abb6cb4a0b5452ed47d3e05d951fe3a4ba2ce
Artem Baguinski <abaguinski@gmail.com>
2026-07-11 08:05:54 +0000

    dry prim integers with a macro

--- src/primint.rs
+++ src/primint.rs
@@ -1,75 +1,37 @@
 use super::{FibFit, FibInteger};
 
-impl FibInteger for u8 {
-    fn zero() -> Self {
-        0
-    }
+macro_rules! impl_fib_integer {
+    ($(($type:ty, $max_index:expr)),+ $(,)?) => {
+        $(
+            impl FibInteger for $type {
+                fn zero() -> Self {
+                    0
+                }
 
-    fn one() -> Self {
-        1
-    }
+                fn one() -> Self {
+                    1
+                }
 
-    fn fits_fibonacci(index: usize) -> FibFit {
-        if index <= 13 { FibFit::Yes } else { FibFit::No }
-    }
+                fn two() -> Self {
+                    2
+                }
+
+                fn fits_fibonacci(index: usize) -> FibFit {
+                    if index <= $max_index {
+                        FibFit::Yes
+                    } else {
+                        FibFit::No
+                    }
+                }
+            }
+        )+
+    };
 }
 
-impl FibInteger for u16 {
-    fn zero() -> Self {
-        0
-    }
-
-    fn one() -> Self {
-        1
-    }
-
-    fn fits_fibonacci(index: usize) -> FibFit {
-        if index <= 24 { FibFit::Yes } else { FibFit::No }
-    }
-}
-
-impl FibInteger for u32 {
-    fn zero() -> Self {
-        0
-    }
-
-    fn one() -> Self {
-        1
-    }
-
-    fn fits_fibonacci(index: usize) -> FibFit {
-        if index <= 47 { FibFit::Yes } else { FibFit::No }
-    }
-}
-
-impl FibInteger for u64 {
-    fn zero() -> Self {
-        0
-    }
-
-    fn one() -> Self {
-        1
-    }
-
-    fn fits_fibonacci(index: usize) -> FibFit {
-        if index <= 93 { FibFit::Yes } else { FibFit::No }
-    }
-}
-
-impl FibInteger for u128 {
-    fn zero() -> Self {
-        0
-    }
-
-    fn one() -> Self {
-        1
-    }
-
-    fn fits_fibonacci(index: usize) -> FibFit {
-        if index <= 186 {
-            FibFit::Yes
-        } else {
-            FibFit::No
-        }
-    }
+impl_fib_integer! {
+    (u8, 13),
+    (u16, 24),
+    (u32, 47),
+    (u64, 93),
+    (u128, 186),
 }
